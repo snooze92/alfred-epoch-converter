f11818b1b47f69392150a74a3fbe5d34e8e4a4e3
Giovanni <5774411+giovannicoppola@users.noreply.github.com>
2022-03-26 18:58:51 -0400

    Update to Python3

--- epoch.py
+++ epoch.py
@@ -1,18 +1,30 @@
-# encoding: utf-8
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+# Partly cloudy ⛅️  🌡️+48°F (feels +45°F, 56%) 🌬️→7mph 🌗 Sat Mar 26 10:55:59 2022
+
 
 import datetime
 import re
 import subprocess
 import sys
 import time
-from workflow import Workflow, ICON_CLOCK, ICON_NOTE
+import json
+import importlib
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+importlib.reload(sys) #not sure this is needed.
 
-LOGGER = None # Set in the main...
+
+
+#sys.setdefaultencoding('utf-8')
+
 MAX_SECONDS_TIMESTAMP = 10000000000
 MAX_SUBSECONDS_ITERATION = 4
+result = {"items": []}
+
+def log(s, *args):
+    if args:
+        s = s % args
+    print(s, file=sys.stderr)
 
 
 def get_divisor(timestamp):
@@ -25,65 +37,94 @@ def get_divisor(timestamp):
 
 def convert(timestamp, converter):
     divisor = get_divisor(timestamp)
-    LOGGER.debug('Found divisor [{divisor}] for timestamp [{timestamp}]'.format(**locals()))
+    log('Found divisor [{divisor}] for timestamp [{timestamp}]'.format(**locals()))
     if divisor > 0:
         seconds, subseconds = divmod(timestamp, divisor)
         subseconds_str = '{:.9f}'.format(subseconds / float(divisor))
         return converter(seconds).isoformat() + subseconds_str[1:].rstrip('0').rstrip('.')
 
 
-def add_epoch_to_time_conversion(wf, timestamp, descriptor, converter):
+def add_epoch_to_time_conversion(timestamp, descriptor, converter):
     converted = convert(timestamp, converter)
     description = descriptor + ' time for ' + str(timestamp)
     if converted is None:
         raise Exception('Timestamp [{timestamp}] is not supported'.format(**locals()))
     else:
-        LOGGER.debug('Returning [{converted}] as [{description}] for [{timestamp}]'.format(**locals()))
-        wf.add_item(title=converted, subtitle=description, arg=converted, valid=True, icon=ICON_CLOCK)
-
-
-def add_time_to_epoch_conversion(wf, dt, descriptor, converter, multiplier):
+        log('Returning [{converted}] as [{description}] for [{timestamp}]'.format(**locals()))
+        result["items"].append({
+        "title": converted,
+        'subtitle': description,
+        'valid': True,
+        
+        "icon": {
+            "path": 'icons/Clock.icns'
+        },
+        'arg': converted
+            }) 
+        
+def add_time_to_epoch_conversion(dt, descriptor, converter, multiplier):
     converted = str(int((dt - converter(0)).total_seconds() * multiplier))
     description = descriptor + ' epoch for ' + str(dt)
-    wf.add_item(title=converted, subtitle=description, arg=converted, valid=True, icon=ICON_CLOCK)
+    result["items"].append({
+        "title": converted,
+        'subtitle': description,
+        'valid': True,
+        
+        "icon": {
+            "path": 'icons/Clock.icns'
+        },
+        'arg': converted
+            }) 
 
-def attempt_conversions(wf, input, prefix=''):
+    
+
+def attempt_conversions(input, prefix=''):
     try:
-        timestamp = int(input)
-        add_epoch_to_time_conversion(wf, timestamp, '{prefix}Local'.format(**locals()), datetime.datetime.fromtimestamp)
-        add_epoch_to_time_conversion(wf, timestamp, '{prefix}UTC'.format(**locals()), datetime.datetime.utcfromtimestamp)
+        timestamp = float(input)
+        add_epoch_to_time_conversion(timestamp, '{prefix}Local'.format(**locals()), datetime.datetime.fromtimestamp)
+        add_epoch_to_time_conversion(timestamp, '{prefix}UTC'.format(**locals()), datetime.datetime.utcfromtimestamp)
     except:
-        LOGGER.debug('Unable to read [{input}] as an epoch timestamp'.format(**locals()))
+        log('Unable to read [{input}] as an epoch timestamp'.format(**locals()))
 
     try:
         match = re.match('(\d{4}-\d{2}-\d{2})?[ T]?((\d{2}:\d{2})(:\d{2})?(.\d+)?)?', str(input))
         date, time, hour_minutes, seconds, subseconds = match.groups()
         if date or time:
-            dt = datetime.datetime.strptime(
+                dt = datetime.datetime.strptime(
                 (date or datetime.datetime.now().strftime('%Y-%m-%d')) + ' ' +
                 (hour_minutes or '00:00') + (seconds or ':00') +
                 ('.000000' if subseconds is None else subseconds[:7]),
                 '%Y-%m-%d %H:%M:%S.%f'
-            )
+                )
 
-            add_time_to_epoch_conversion(wf, dt, '{prefix}Local s.'.format(**locals()), datetime.datetime.fromtimestamp, 1)
-            add_time_to_epoch_conversion(wf, dt, '{prefix}Local ms.'.format(**locals()), datetime.datetime.fromtimestamp, 1e3)
-            add_time_to_epoch_conversion(wf, dt, u'{prefix}Local µs.'.format(**locals()), datetime.datetime.fromtimestamp, 1e6)
-            add_time_to_epoch_conversion(wf, dt, '{prefix}Local ns.'.format(**locals()), datetime.datetime.fromtimestamp, 1e9)
+                add_time_to_epoch_conversion(dt, '{prefix}Local s.'.format(**locals()), datetime.datetime.fromtimestamp, 1)
+                add_time_to_epoch_conversion(dt, '{prefix}Local ms.'.format(**locals()), datetime.datetime.fromtimestamp, 1e3)
+                add_time_to_epoch_conversion(dt, '{prefix}Local us.'.format(**locals()), datetime.datetime.fromtimestamp, 1e6)
+                add_time_to_epoch_conversion(dt, '{prefix}Local ns.'.format(**locals()), datetime.datetime.fromtimestamp, 1e9)
 
-            add_time_to_epoch_conversion(wf, dt, '{prefix}UTC s.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1)
-            add_time_to_epoch_conversion(wf, dt, '{prefix}UTC ms.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e3)
-            add_time_to_epoch_conversion(wf, dt, u'{prefix}UTC µs.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e6)
-            add_time_to_epoch_conversion(wf, dt, '{prefix}UTC ns.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e9)
+                add_time_to_epoch_conversion(dt, '{prefix}UTC s.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1)
+                add_time_to_epoch_conversion(dt, '{prefix}UTC ms.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e3)
+                add_time_to_epoch_conversion(dt, '{prefix}UTC us.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e6)
+                add_time_to_epoch_conversion(dt, '{prefix}UTC ns.'.format(**locals()), datetime.datetime.utcfromtimestamp, 1e9)
     except:
-        LOGGER.debug('Unable to read [{input}] as a human-readable datetime'.format(**locals()))
+        log('Unable to read [{input}] as a human-readable datetime'.format(**locals()))
 
 
-def add_current(wf, unit, multiplier):
+def add_current(unit, multiplier):
     converted = str(int(time.time() * multiplier))
     description = 'Current timestamp ({unit})'.format(**locals())
-    LOGGER.debug('Returning [{converted}] as [{description}]'.format(**locals()))
-    wf.add_item(title=converted, subtitle=description, arg=converted, valid=True, icon=ICON_NOTE)
+    log('Returning [{converted}] as [{description}]'.format(**locals()))
+    result["items"].append({
+        "title": converted,
+        'subtitle': description,
+        'valid': True,
+        
+        "icon": {
+            "path": 'icons/AlertNoteIcon.icns'
+        },
+        'arg': converted
+            }) 
+    
 
 
 def get_clipboard_data():
@@ -92,27 +133,26 @@ def get_clipboard_data():
     return p.stdout.read()
 
 
-def main(wf):
-    if len(wf.args) > 0:
-        query = wf.args[0]
+def main():
+    if len(sys.argv) > 1:
+        query = sys.argv[1]
         if query:
-            LOGGER.debug('Got query [{query}]'.format(**locals()))
-            attempt_conversions(wf, query)
+            log('Got query [{query}]'.format(**locals()))
+            attempt_conversions(query)
+            
 
     clipboard = get_clipboard_data()
     if clipboard:
-        LOGGER.debug('Got clipboard [{clipboard}]'.format(**locals()))
-        attempt_conversions(wf, clipboard, prefix='(clipboard) ')
+        log('Got clipboard [{clipboard}]'.format(**locals()))
+        attempt_conversions(clipboard, prefix='(clipboard) ')
 
-    add_current(wf, 's', 1)
-    add_current(wf, 'ms', 1e3)
-    add_current(wf, u'µs', 1e6)
-    add_current(wf, 'ns', 1e9)
-
-    wf.send_feedback()
+    add_current('s', 1)
+    add_current('ms', 1e3)
+    add_current('µs', 1e6)
+    add_current('ns', 1e9)
+    print (json.dumps(result))
+    
 
 
 if __name__ == u"__main__":
-    wf = Workflow()
-    LOGGER = wf.logger
-    sys.exit(wf.run(main))
+    main ()
